f01c51fd6bc6a1d423aed745cc6015d08ba3f419
ccdv2 <denghaoyuan1100@126.com>
2019-02-28 22:22:13 +0800

    first try to remember.. Failed

--- GuessWordGame/loginwindow.cpp
+++ GuessWordGame/loginwindow.cpp
@@ -1,6 +1,17 @@
 #include "loginwindow.h"
+#include <QVBoxLayout>
+
 
 LoginWindow::LoginWindow(QWidget *parent) : QWidget(parent)
 {
+	userName = new QLineEdit(this);
+	passWord = new QLineEdit(this);
+	submitButton = new QPushButton(this);
 
+	QVBoxLayout *loginWindowLayout = new QVBoxLayout(this);
+	loginWindowLayout->addWidget(userName);
+	loginWindowLayout->addWidget(passWord);
+	loginWindowLayout->addWidget(submitButton);
+
+	setLayout(loginWindowLayout);
 }
--- GuessWordGame/mainwindow.cpp
+++ GuessWordGame/mainwindow.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
+#include <QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	setWindowTitle(tr("猜单词游戏"));
-	resize(1600, 900);
+	resize(2560, 1440);
+
+	loginWindow = new LoginWindow(this);
+
+	QGridLayout *mainLayout = new QGridLayout(this);
+	mainLayout->addWidget(loginWindow, 0, 0);
+
+	setLayout(mainLayout);
 }
 
 MainWindow::~MainWindow()
